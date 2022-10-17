26d55ef2b8b965ed780c2e148dee52ac95efc31d
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-17 07:40:28 +0000

    Made the number to guess random

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #day 18 of 100-days-of-code
 print("Guess my number between 0 - 1,000,000")
 
-correct_number = 42
-attempt = 1 
+import random
+attempt = 1
+correct_number = random.randint(1,1000000)
+print(correct_number)#this is for bug fixing if you run the code you will see why
 
 while True:
 	guess =int(input("Guess my number: "))
@@ -13,7 +15,7 @@ while True:
 		print("That number is too low. Try again!")
 		attempt += 1
 	elif guess > correct_number:
-		print("That number is too low. Try again!")
+		print("That number is too high. Try again!")
 		attempt += 1
 	elif guess == correct_number:
 		print("You are a winner!!")
